f512c5a63a97cf4648d159fb1c621ef70f63e1c2
Orloaft <alexorlow17@gmail.com>
2022-04-04 18:52:49 -0400

    add an input element with the autocomplete functionality

---
+++ assets/js/autocomplete.js
@@ -0,0 +1,24 @@
+let autocomplete;
+function initAutocomplete() {
+  autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById("autocomplete"),
+    {
+      types: ["establishment"],
+      componentRestrictions: { country: ["CA", "US", "AU"] },
+      fields: ["place_id", "geometry", "name"],
+    }
+  );
+  autocomplete.addListener("place_changed", onPlaceChanged);
+}
+
+function onPlaceChanged() {
+  let place = autocomplete.getPlace();
+
+  if (!place.geometry) {
+    //user did not select a prediction; reset input field
+    document.getElementById("autocomplete").placeholder = "enter a place";
+  } else {
+    //display details about a valid place
+    document.getElementById("details").innerHTML = place.name;
+  }
+}
