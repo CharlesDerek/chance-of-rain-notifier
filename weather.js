bf59dcccfc120a3b3ee63b922ae011d977ebd053
Orloaft <alexorlow17@gmail.com>
2022-04-05 00:52:09 -0400

    add partial code for google map

--- weather.js
+++ weather.js
@@ -1,3 +1,4 @@
+// function that accepts an address and console longs the coordinates
 function geocode(address) {
   axios
     .get("https://maps.googleapis.com/maps/api/geocode/json", {
@@ -18,9 +19,11 @@ function geocode(address) {
 document.getElementById("address-form").addEventListener("submit", (event) => {
   event.preventDefault();
   let givenAddress = event.target.addressInput.value;
-
   geocode(givenAddress);
+  event.target.addressInput.value = "";
 });
+
+//Autocomplete function for the input field, checks only for real geographical locations
 let autocomplete;
 function initAutocomplete() {
   autocomplete = new google.maps.places.Autocomplete(
@@ -43,3 +46,25 @@ function onPlaceChanged() {
     //display details about a valid place
   }
 }
+
+//map code starts here
+
+//Initialize and add the map
+// function initMap() {
+//   // The location of Uluru
+//   const uluru = { lat: -35.344, lng: -70.036 };
+//   // The map, centered at Uluru
+//   const map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 8,
+//     center: uluru,
+//   });
+//   // The marker, positioned at Uluru
+//   const marker = new google.maps.Marker({
+//     position: uluru,
+//     map: map,
+//   });
+// }
+const map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 8,
+  center: uluru,
+});
