a995ed6c02a746e0ea5d4e3439c32875082f562b
Kykeon <paulinoandresmiguel@gmail.com>
2022-04-05 10:42:01 -0400

    Change buteler logo and add Send Flowers font

--- index.js
+++ index.js
@@ -87,7 +87,6 @@ function initMap() {
 document.addEventListener('DOMContentLoaded', () => requestAnimationFrame(updateTime));
 
 function updateTime() {
-  document.documentElement.style.setProperty('--timer-day', "'" + moment().format('dd') + "'");
   document.documentElement.style.setProperty('--timer-hours', "'" + moment().format('k') + "'");
   document.documentElement.style.setProperty('--timer-minutes', "'" + moment().format('mm') + "'");
   document.documentElement.style.setProperty('--timer-seconds', "'" + moment().format('ss') + "'");
